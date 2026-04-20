c9255580bf48831d4c90b38438224659b454a35c
Saleh <salehmnasra@gmail.com>
2026-04-20 19:14:14 +0300

    Add: combination-target-sum - submission-1

---
+++ Data Structures & Algorithms/combination-target-sum/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+
+
+        def bt(i,res,curr):
+            if sum(curr)==target:
+                if sorted(curr) not in res:
+                    curr = sorted(curr)
+                    res.append(curr[:])
+                return
+
+            elif sum(curr) > target or i==len(nums):
+                return
+            else:
+                curr.append(nums[i])
+                bt(i,res,curr)
+                curr.pop()
+                bt(i+1,res,curr)
+        res=[]
+        bt(0,res,[])
+        return res
